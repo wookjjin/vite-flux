00ba3c620677c312f5bf6d3bb818872b8c8c4baf
JinWook <jwook0318@gmail.com>
2026-03-24 14:11:55 +0900

    Change react swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
